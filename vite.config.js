4dcc39d984081077d177d27e7eb292277318aaf6
2303A52021 <160995904+2303A52021@users.noreply.github.com>
2025-10-24 11:58:34 +0530

    Configure Vite for React with base path

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/RL-PROJECT-2/',   // 👈 must match your repo name
+})
